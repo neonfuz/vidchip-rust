9f81751e4b04b9a22dd7802bccbf09f27c7196ba
neonfuz <neonfuz@gmail.com>
2017-03-02 20:23:52 -0600

    Added window creation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 extern crate sdl2;
 
-fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let mut event_pump = sdl_context.event_pump().unwrap();
+use sdl2::event::Event;
 
-    for event in event_pump.poll_iter() {
+fn main() {
+    let sdl = sdl2::init().unwrap();
+    let mut event_pump = sdl.event_pump().unwrap();
+    let video = sdl.video().unwrap();
+
+    let window = video.window("chip", 320, 240).build().unwrap();
+
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} => { break 'main; }
+                _ => {}
+            }
+        }
         
+
     }
 
     println!("Hello, world!");
