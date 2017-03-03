30579ee645de06e505befd8f29e344fe984cbe1a
neonfuz <neonfuz@gmail.com>
2017-03-02 18:31:10 -0600

    Added SDL to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate sdl2;
+
 fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    for event in event_pump.poll_iter() {
+        
+    }
+
     println!("Hello, world!");
 }
